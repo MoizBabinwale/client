cd89f21cf09100b10f0624228e67bb3e974aad9c
MoizBabinwale <mbabinwale@gmail.com>
2022-12-03 16:29:03 +0530

    front end

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,52 +1,41 @@
 import React from 'react'
 import './LeftSidebar.css'
 import { NavLink } from 'react-router-dom'
-import Globe from  '../../assets/Globe.svg'
 import { useState } from 'react'
+import Globe from  '../../assets/Globe.svg'
 
 
-const LeftSidebar = () => {
-    const [showMediaicon, setshowMediaicon] = useState(true);
+    //Using Inline Function and the The Logical Not (!) to toggle state
+    const LeftSidebar = () => {
+        const [icon, menu] = useState(false)
     return (
-        <div className='left-sidebar'>
-        {/* <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> */}
-
-        {/* </link> */}
+            <div className='left-sidebar'>
+        <button id="menu" onClick={() => menu(!icon)}>
+            <div className="bar" id="bar1"></div>
+            <div className="bar" id="bar2"></div>
+            <div className="bar" id="bar3"></div>
+         </button>
+         {icon &&(
             <nav className='side-nav'>
-                <NavLink to='/' className="side-nav-links"activeclassname="active"  >
+                <NavLink to='/' className='side-nav-links' activeclassname='active'>
                     <p>Home</p>
                 </NavLink>
                 <div className='side-nav-div'>
                     <div><p>PUBLIC</p></div>
-                    <NavLink to='/Questions' className= "side-nav-links" activeclassname="active" >
+                    <NavLink to='/Questions' className='side-nav-links' activeclassname='active'>
                         <img src={Globe} alt="Globe" />
                         <p style={{paddingLeft: "10px"}}> Questions </p>
                     </NavLink>
-                    <NavLink to='/Tags' className="side-nav-links"  activeclassname="active" style={{paddingLeft: "40px"}}>
+                    <NavLink to='/Tags' className='side-nav-links' activeclassname='active' style={{paddingLeft: "40px"}}>
                         <p>Tags</p>
                     </NavLink>
-                    <NavLink to='/Users' className="side-nav-links" activeclassname="active"   style={{paddingLeft: "40px"}}>
+                    <NavLink to='/Users' className='side-nav-links' activeclassname='active' style={{paddingLeft: "40px"}}>
                         <p>Users</p>
                     </NavLink>
                 </div>
             </nav>
-                <div className="burger" >
-                <li className="line" onClick={() => setshowMediaicon(!showMediaicon)}></li>
-                <li className="line"></li>
-                <li className="line"></li>
-                
-        
-        
-                </div>
-                <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-                <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-               
-      </div>
-      
+            )}
+        </div>
     )
 }
 
